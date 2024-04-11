53a99b8d7d176c32176739ccc2520d2b057af8d6
vanes34 <vanesabinaj26@gmail.com>
2024-04-11 22:05:52 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+document.getElementById("read_more").addEventListener("click", changeClass);
+
+function changeClass() {
+  var content = document.getElementById("extra_content");
+  var btn = document.getElementById("read_more");
+  content.classList.toggle("show");
+
+  if (content.classList.contains("show")) {
+    btn.innerHTML = "Show Less";
+  } else {
+    btn.innerHTML = "Show More";
+  }
+}
+
+const hamMenu = document.querySelector(".ham-menu");
+
+const offScreenMenu = document.querySelector(".nav-links");
+
+hamMenu.addEventListener("click", () => {
+  hamMenu.classList.toggle("active");
+  offScreenMenu.classList.toggle("active");
+});
